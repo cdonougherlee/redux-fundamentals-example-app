a16a1d65835a743fa8e038f6d11484b77a22a268
cdonougherlee <cdonougherlee@gmail.com>
2024-02-14 12:18:20 +1300

    idk it broke

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { Provider } from 'react-redux'
 
-import './api/server'
+import App from './App'
+import store from './store'
 
 ReactDOM.render(
+  // Render a `<Provider>` around the entire `<App>`,
+  // and pass the Redux store to it as a prop
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
